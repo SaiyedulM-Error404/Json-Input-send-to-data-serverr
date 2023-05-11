c0eadb9178135954376cebdfe2e781943301246a
SaiyedulM-Error404 <mdsaiyedulmorsalin.code@gmail.comgit config --global user.email>
2023-05-12 00:33:50 +0600

    this is last update for all

---
+++ script.js
@@ -0,0 +1,8 @@
+const userName = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+const submitBtn = document.getElementById("btn");
+submitBtn.addEventListener("click",function(){
+    console.log("Click Marse");
+    
+})
